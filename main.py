4e75d2df8e450e5c87b250f9ffedf131475a1319
Judy@LAPTOP <judylight2001@gmail.com>
2018-04-13 16:17:39 -0700

    final changes

--- main.py
+++ main.py
@@ -7,57 +7,55 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-username=""
-password=""
-verify_password=""
-email=""
+# username=""
+# password=""
+# verify_password=""
+# email=""
 
 
 @app.route("/validation", methods=['POST'])
 def validation():
+    username=request.form['username']
+    password=request.form['password']
+    verify_password=request.form['verify_password']
+    email=request.form['email']
+
     usernameError=""
     passwordError=""
     verifyError=""
     emailError=""
 
-    if username != ' ' and len(username) >2 and len(username) < 21:
-        username=username
-    else:
-        username == ''
-        usernameError = "Username should consist of 3-20 alphanumeric characters with no spaces"
-    return username, usernameError
+    # get_info = request.form['get_info']
 
-    if password != ' ' and len(password) > 2 and len(password) < 21:
-        password == ''
-    else:
+    if username == ' ' or len(username) <3 or len(username) >20:
+        usernameError = "Username should consist of 3-20 alphanumeric characters with no spaces"
+    
+    if password == ' ' or len(password) <3 or len(password) >20:
         password == ''
         passwordError = "Password should consist of 3-20 characters with no spaces"
-    return password, passwordError
 
-    if verify_password!= ' ' and password != verify_password:
-        verify_password == ''
-    else:
+    if verify_password == ' ' or password != verify_password:
         verify_password == ''
         verifyError = "Passwords must match"
-    return verify_password, verifyError
 
-    if len(email) > 2 and len(email) < 21 and '@' in email and '.' in email:
-        email = email
+    if email == '':
+        email == email
     else:
-        email = ''
-        emailError = "Please enter a valid email (should include '@' and '.')"
-    return email, emailError
+        if len(email) <3 or len(email) >20:
+            emailError = "Email should be between 3 and 20 characters in length"
+        elif '@' not in email or '.' not in email:
+            email == ''
+            emailError = "Email should include '@' and '.'"
 
     if  not usernameError and not passwordError and not verifyError and not emailError:
         return render_template('welcome.html', username=username)
     else:
-        return render_template('index.html', usernameError=usernameError, passwordError=passwordError, verifyError=verifyError, emailError=emailError)
+        return render_template('index.html', usernameError=usernameError, passwordError=passwordError, 
+            verifyError=verifyError, emailError=emailError, username=username, email=email)
 
 
-
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-
     return render_template('index.html')
 
 app.run()
